a4562a46c7f1fefc559652c256ded1f16f2cbcbe
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-08-18 22:06:15 +1000

    fix(global.css): uncomment the commented codes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,35 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
 export default function AboutPage() {
     return (
-        <section>
-            about
-        </section>
+        <main className="h-screen flex flex-col justify-between">
+            <Header />
+            <section>
+                <div>
+                    <h1>What is it?</h1>
+                    <p>
+                        The word Ngringkes is originally come from Javanese word which mean summarize.
+                        Ngringkes is a tool that convert podcast into a clean sumamrized transcript without listening to the whole thing
+                    </p>
+                </div>
+                <div>
+                    <h1>But why?</h1>
+                    <p>
+                        I built this because I have little time to sit throught full lecture recordings or the world economy and AI related podcasts on Youtube that I wanted to keep up with.
+                        This tool help me to transcribe and summarize it, so I can catch up in minutes
+                    </p>
+                </div>
+                <div>
+                    <h1>How it works?</h1>
+                    <p>TODO</p>
+                </div>
+                <div>
+                    <h1>Tech stack</h1>
+                    <p>Built with Next.js, Tailwind CSS, and Gemini API</p>
+                </div>
+            </section>
+            <Footer />
+        </main>
     )
 }
