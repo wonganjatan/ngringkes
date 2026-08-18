015c6f9295e1cb35c7e508b2683004d1bc7018da
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-08-18 21:39:05 +1000

    style(Header.tsx): imporve the header background color and add border style to the nav buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,19 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <section className="flex items-center justify-between px-8 py-4 bg-linear-to-br from-blue-500 to-red-500">
+        <section className="flex items-center justify-between px-8 py-4 bg-linear-to-br from-indigo-600 to-purple-500">
             <div>
-                <Link href="/" className="text-2xl">Ngringkes</Link>
+                <Link href="/" className="text-white text-2xl">Ngringkes</Link>
             </div>
-            <div className="flex">
-                <Link href="/podcasts">Your Summarized Podcast</Link>
-                <Link href="/podcasts/new">Summarize New Podcast</Link>
-                <Link href="/about">About</Link>
-            </div>
+            <nav className="flex items-center justify-center gap-2">
+                <Link href="/podcasts" className="flex flex-col text-white border-2 border-transparent hover:border-b-red-500 transition-colors duration-300">
+                    Your Podcasts
+                </Link>
+                <Link href="/podcasts/new" className="flex flex-col text-white border-2 border-transparent hover:border-b-yellow-500 transition-colors duration-300">
+                    New Podcast
+                </Link>
+                <Link href="/about" className="text-white border-2 border-transparent hover:border-b-green-500 transition-colors duration-300">About</Link>
+            </nav>
         </section>
     )
 }
